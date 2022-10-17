55e55fe5fa599472e91f362a9fc4497eb3c67b53
Weverton18 <wevertonardo17@gmail.com>
2022-10-17 11:14:56 -0300

    Correção Nova

--- BetaCliente1.php
+++ BetaCliente1.php
@@ -16,16 +16,14 @@
 	if($Action == 'salvar')
 	{	
 		try {
-		$sql = "insert into cliente (login, Nome, Apelido, Telefone) values ( ?,?,?,? )";
-		$stmt = $conn->prepare($sql);
-		 $stmt->execute(array(
-		    $usuario, $nome, $apelido, $Tel
-		  ));
-		header('location: BetaCliente1.php');
-
-		 } catch(PDOException $e) {
-				echo 'Error: ' . $e->getMessage();
-			}
+			$sql = "INSERT INTO cliente (login, Nome, Apelido, Telefone) values ( ?,?,?,? )";
+			$stmt = $conn->prepare($sql);
+			$stmt->execute(array($usuario, $nome, $apelido, $Tel));
+			header('location: BetaCliente1.php');
+			exit;
+		} catch(PDOException $e) {
+			echo 'Error: ' . $e->getMessage();
+		}
 	}
 	else
 	if ($Action == 'editar')
